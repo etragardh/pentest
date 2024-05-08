cc3a50dc34d2029a21bcbe7756fcb76481700860
Emil Trägårdh <emil@tstck.io>
2024-05-08 14:30:32 +0200

    update oxygen readme and dynamic rce

---
+++ wp/plugins/yoast-seo/22.5/vendor/composer/installed.php
@@ -0,0 +1,32 @@
+<?php return array(
+    'root' => array(
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'type' => 'wordpress-plugin',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'reference' => 'dcec13de30915e04f3a6fffe25e390b26137f81d',
+        'name' => 'yoast/wordpress-seo',
+        'dev' => false,
+    ),
+    'versions' => array(
+        'composer/installers' => array(
+            'pretty_version' => 'v2.2.0',
+            'version' => '2.2.0.0',
+            'type' => 'composer-plugin',
+            'install_path' => __DIR__ . '/./installers',
+            'aliases' => array(),
+            'reference' => 'c29dc4b93137acb82734f672c37e029dfbd95b35',
+            'dev_requirement' => false,
+        ),
+        'yoast/wordpress-seo' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'type' => 'wordpress-plugin',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'reference' => 'dcec13de30915e04f3a6fffe25e390b26137f81d',
+            'dev_requirement' => false,
+        ),
+    ),
+);
---
+++ wp/plugins/yoast-seo/22.6/vendor/composer/installed.php
@@ -0,0 +1,41 @@
+<?php return array(
+    'root' => array(
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'type' => 'wordpress-plugin',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'reference' => 'a73019d649e2dc8bc3d538b0a86b02d41c89b935',
+        'name' => 'yoast/wordpress-seo',
+        'dev' => false,
+    ),
+    'versions' => array(
+        'composer/installers' => array(
+            'pretty_version' => 'v2.2.0',
+            'version' => '2.2.0.0',
+            'type' => 'composer-plugin',
+            'install_path' => __DIR__ . '/./installers',
+            'aliases' => array(),
+            'reference' => 'c29dc4b93137acb82734f672c37e029dfbd95b35',
+            'dev_requirement' => false,
+        ),
+        'yoast/whip' => array(
+            'pretty_version' => '2.0.0',
+            'version' => '2.0.0.0',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../yoast/whip',
+            'aliases' => array(),
+            'reference' => '5cfd9c3b433774548ec231fe896d5e85d17ed0d1',
+            'dev_requirement' => false,
+        ),
+        'yoast/wordpress-seo' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'type' => 'wordpress-plugin',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'reference' => 'a73019d649e2dc8bc3d538b0a86b02d41c89b935',
+            'dev_requirement' => false,
+        ),
+    ),
+);
